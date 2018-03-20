eaf9a44400abdd96818498a77970f2b1c3583165
Andreza Feu <andrezafeu@gmail.com>
2018-03-19 20:59:19 -0400

    Add :title route and page_content method

--- wiki.rb
+++ wiki.rb
@@ -1,6 +1,17 @@
 require "sinatra"
 
+def page_content(title)
+  File.read("pages/#{title}.txt")
+# if the file is not found, raise exception
+rescue Errno::ENOENT
+  return nil
+end
+
 get "/" do
   # the erb method returns the result as string
   erb :welcome
+end
+
+get "/:title" do
+  page_content(params[:title])
 end
