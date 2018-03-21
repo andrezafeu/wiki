3bfc0f88cb5056bef5bc27f303e7b56748dbab40
Andreza Feu <andrezafeu@gmail.com>
2018-03-20 22:22:47 -0400

    Add form, routes and method to create a new wiki

--- wiki.rb
+++ wiki.rb
@@ -7,9 +7,26 @@ rescue Errno::ENOENT
   return nil
 end
 
+def save_content(title, content)
+  File.open("pages/#{title}.txt", "w") do |file|
+    file.print(content)
+  end
+end
+
 get "/" do
   # the erb method returns the result as string
   erb :welcome
+end
+
+get "/new" do
+  erb :new
+end
+
+post "/create" do
+  save_content(params[:title], params[:content])
+  # get request to show the new page after it's saved
+  redirect "/#{params[:title]}"
+  
 end
 
 get "/:title" do
